d4ce844cbe392a7e7cd99662aa07954bda6b0027
WaleedUkhan <wk3132162@gmail.com>
2025-09-19 20:54:47 +0500

    DayOne Completed

--- DayOne/One.js
+++ DayOne/One.js
@@ -11,3 +11,13 @@ console.log(fruits.indexOf("Orange"));
 fruits.pop();
 console.log(fruits);
 
+//Console.log is a window function 
+// while process.stdout.write is NodeJs function 
+// ctrl + ~ Opens terminal for NodeJS 
+// Prompt() Opens box for windows but what for NodeJS? 
+// for opening that input box in terminal we need to install this package: prompt-sync and wil need to require it in code
+
+let inputBox = require('prompt-sync')();
+let myBox = inputBox("Enter your Age: "); 
+// console.log(myBox);
+ process.stdout.write("Your Age is  ", myBox);
