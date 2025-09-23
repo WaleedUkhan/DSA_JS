d1f9fd2a6552dc89dfbb6779c68d8af0b5287042
WaleedUkhan <wk3132162@gmail.com>
2025-09-23 10:21:52 +0500

    Reverse Loop and Patterns

--- DayOne/Two.js
+++ DayOne/Two.js
@@ -2,9 +2,50 @@ let num = require("prompt-sync")();
 let myNum = Number(num("Enter your Number: "));
 console.log(`My entered num is : ${myNum}`);
 
-for(let i = 1 ; i<= myNum; i++){
-    for(let j=1; j<=myNum; j++){
-        process.stdout.write("*  "); 
+// for(let i = 1 ; i<= myNum; i++){
+//     for(let j=1; j<=myNum; j++){
+//         process.stdout.write("*  "); 
+//     }
+//     console.log();
+// }
+
+
+// for(let i = 1 ; i<= myNum; i++){
+//     for(let j=1; j<=i ; j++){
+//         process.stdout.write("*  "); 
+//     }
+//     console.log();
+// }
+
+
+// for(let i = 1 ; i<= myNum; i++){
+//     for(let j=1; j<=i ; j++){
+//         process.stdout.write(j + " "); 
+//     }
+//     console.log();
+// }
+
+// for(let i = 1 ; i<= myNum; i++){
+//     let char = 65; 
+//     for(let j=1; j<=i ; j++){
+//         process.stdout.write(String.fromCharCode(char) + " "); 
+//         char++
+//     }
+//     console.log();
+// }
+
+// for(let i = myNum ; i >= 1 ; i--){
+//     console.log(i); 
+// }
+
+
+// Method ONE
+
+for(let i = 1 ; i <= myNum ; i++){
+    for(let j = myNum; j >= i ; j--){
+         process.stdout.write("*  "); 
     }
-    console.log();
+    console.log(); 
 }
+
+
